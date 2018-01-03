8e6a34c48a6e07ffcb0f06d94d713f23216a9f94
Jarrod Worlitz <jarrod.worlitz@gmail.com>
2018-01-03 00:02:31 -0800

    angler: OPM3.171019.013 Generated using https://github.com/anestisb/android-prepare-vendor Based on marlin blobs extraction cleanup by mydongistiny

--- angler/BoardConfigVendor.mk
+++ angler/BoardConfigVendor.mk
@@ -1,14 +1,20 @@
-# Copyright (C) 2016 The Pure Nexus Project
+#
+# Copyright (C) 2017 Benzo Rom
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+TARGET_BOARD_INFO_FILE := vendor/huawei/angler/vendor-board-info.txt
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_VENDORIMAGE_PARTITION_SIZE := 209715200
+# Bypass location API missing dependencies AOSP compilation problem
+# Target shared libs are included as pre-build from factory image
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE :=
